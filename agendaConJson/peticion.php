6e6633eef5fd2c64371882fbf226a5d67d91948d
Alberto Calzada <alberto.calzada2@gmail.com>
2023-10-24 13:35:41 +0200

    Completando peticion json

--- agendaConJson/peticion.php
+++ agendaConJson/peticion.php
@@ -10,25 +10,23 @@
     <?php
 
     //$url = "192.168.4.224/";
-   $url = "192.168.4.243/2-daw/Servidor/PHP/Ejercicios/Ejercicio/AgendaJSON/saveAgenda.php"; 
+    $url = "192.168.4.228/servidor/agenda_json/saveagenda.php";
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE);
     $json = curl_exec($curl);
     curl_close($curl);
 
     $objeto_json = json_decode($json);
-    for ($i=0; $i <count($objeto_json) ; $i++) { 
-        echo $objeto_json[$i]->name."\n";
-        echo $objeto_json[$i]->surname."\n";
-        echo $objeto_json[$i]->tel."\n";
+    for ($i = 0; $i < count($objeto_json); $i++) {
+        echo $objeto_json[$i]->nombre . "\n";
+        echo $objeto_json[$i]->apellido . "\n";
+        echo $objeto_json[$i]->telefono . "\n";
         echo "<br>";
     }
-   
+
     //var_dump($objeto_json);
     ?>
 
 </body>
 
 </html>
-
-
