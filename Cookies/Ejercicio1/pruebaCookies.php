f9aef47d69c03f93f5bafb4e5b0af9d85d1546d9
Alberto Calzada <alberto.calzada2@gmail.com>
2023-10-31 11:42:03 +0100

    terminando prueba cookies

--- Cookies/Ejercicio1/pruebaCookies.php
+++ Cookies/Ejercicio1/pruebaCookies.php
@@ -8,7 +8,7 @@
 </head>
 
 <body>
-    <form action="./index.php" method="POST">
+    <form action="./pruebaCookies.php" method="POST">
         <select name="clothes" id="clothes">
             <option value="elegir">Elegir</option>
             <option value="camiseta">Camisetas</option>
@@ -16,8 +16,11 @@
             <input type="submit" value="Enviar">
         </select>
     </form>
+
     <?php
-    if(isset($_POST["clothes"]) || isset($_COOKIE["Like"])){
+ 
+    if(isset($_POST["clothes"]))
+    {
         if (($fp = fopen("./csv/ropa.csv", "r")) != FALSE) {
             while (($data = fgetcsv($fp, 1000, ",")) != FALSE) {
                 $tipo = $data[0];
@@ -25,27 +28,30 @@
                 $color = $data[2];
                 if(isset($_POST["clothes"]) && $tipo == $_POST["clothes"]){
                     echo "$tipo, $talla, $color <br>";
-                } else if(isset($_COOKIE["Like"]) && $_COOKIE["Like"] == $tipo){
+                } 
+            }
+        }
+
+        fclose($fp);
+        $cookie_value = $_POST["clothes"];
+        setcookie("ropa",$cookie_value,time()+3600);
+    }
+
+    if(isset($_COOKIE["ropa"]) && !isset($_POST["clothes"]))
+    {
+        if (($fp = fopen("./csv/ropa.csv", "r")) != FALSE) {
+            while (($data = fgetcsv($fp, 1000, ",")) != FALSE) {
+                $tipo = $data[0];
+                $talla = $data[1];
+                $color = $data[2];
+                if(isset($_COOKIE["ropa"]) && $tipo == $_COOKIE["ropa"]){
                     echo "$tipo, $talla, $color <br>";
-                }
+                } 
             }
         }
     }
-    if(isset($_POST["clothes"])){
-        $cookie_name = "Like";
-        $cookie_value = $_POST["clothes"];
-        setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/");
-    }
-
-    /*while($line = fgets($fp)){
-        $tipo = explode(",", $line)[0];
-        $talla = explode(",", $line)[1];
-        $color = explode(",", $line)[2];
-    }*/
-    
     ?>
-    
-        
+
 </body>
 
 </html>
