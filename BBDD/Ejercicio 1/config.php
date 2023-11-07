d6121afffb5fadd6ea0a8cf2d1ebcdd144790d8b
Alberto Calzada <alberto.calzada2@gmail.com>
2023-11-07 18:38:35 +0100

    creacion nueva bbdd modificada

---
+++ BBDD/Ejercicio 1/config.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+  $conn = new PDO("mysql:host=$servername", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // Crear la base de datos
+  $nombreBBDD = "alumnos";
+  $sql = "CREATE DATABASE IF NOT EXISTS $nombreBBDD";
+  $conn->exec($sql);
+  echo "Database created successfully<br>";
+
+  // Seleccionar la base de datos recién creada
+  $conn->exec("USE $nombreBBDD");
+
+  // Crear la tabla 'alumno'
+  $sql = "CREATE TABLE IF NOT EXISTS alumno (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(30) NOT NULL,
+    lastname VARCHAR(30) NOT NULL
+  )";
+  $conn->exec($sql);
+  echo "Table 'alumno' created successfully";
+} catch(PDOException $e) {
+  echo "Error: " . $e->getMessage();
+}
+
+
+?>
---
+++ BBDD/Ejercicio 1/metodos.php
@@ -0,0 +1,38 @@
+<?php
+
+function Insertar($conn, $firstname, $lastname)
+{
+    try {
+        $sql = "INSERT INTO alumno (firstname, lastname) VALUES ('$firstname', '$lastname')";
+        $conn->exec($sql);
+        echo "New record created successfully";
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
+}
+
+
+function ListarAlumnos($conn)
+{
+    $sql = "SELECT * FROM alumno";
+    $stmt = $conn->query($sql);
+    $alumnos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    if (count($alumnos) > 0) {
+        echo "<table>";
+        echo "<tr><th>Nombre</th><th>Apellido</th></tr>";
+
+        foreach ($alumnos as $alumno) {
+            echo "<tr>";
+            echo "<td>" . $alumno["firstname"] . "</td>";
+            echo "<td>" . $alumno["lastname"] . "</td>";
+            echo "</tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "0 results";
+    }
+}
+
+
